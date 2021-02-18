9c3f7580bf1e56cf150ddefd9ad95a0bb7a1f14f
Khan Tanveer <khantanveer7977@gmail.com>
2021-02-18 15:10:52 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./Data/Data.json";
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div
+        className="app__input"
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      >
+        <input type="text" placeholder="search" />
+      </div>
+      {data
+        .filter((val) => {
+          if (input === "") {
+            return val;
+          } else if (
+            val.first_name.toLowerCase().includes(input.toLowerCase()) ||
+            val.last_name.toLowerCase().includes(input.toLowerCase())
+          ) {
+            return val;
+          }
+        })
+        .map((item) => {
+          return (
+            <div key={item.id}>
+              <h3>
+                {item.first_name} {item.last_name}
+              </h3>
+
+              <p>{item.email}</p>
+              {item.gender}
+            </div>
+          );
+        })}
     </div>
   );
-}
+};
 
 export default App;
